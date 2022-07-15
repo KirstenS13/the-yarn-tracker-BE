8eee6976b82319531124949db40299e53703e1b6
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-07-14 23:48:09 -0500

    made the initial migration async, rolled it back, and reran it, because it had not properly executed the first time

--- data/migrations/20220630175001_initial.js
+++ data/migrations/20220630175001_initial.js
@@ -1,23 +1,25 @@
-exports.up = function(knex) {
-    knex.schema.createTable('user', (table) => {
+// migration files must be async for them to work properly
+
+exports.up = async function(knex) {
+    await knex.schema.createTable('user', (table) => {
         table.uuid('id').primary().unique().notNullable()
         table.text('username').unique().notNullable()
         table.text('password').notNullable()
         table.text('email').unique().notNullable()
     })
 
-    knex.schema.createTable('colorway', (table) => {
+    await knex.schema.createTable('colorway', (table) => {
         table.uuid('id').primary().unique()
         table.text('name').notNullable()
         table.text('image_path')
     })
 
-    knex.schema.createTable('fiber', (table) => {
+    await knex.schema.createTable('fiber', (table) => {
         table.uuid('id').primary().unique().notNullable()
         table.text('name').unique().notNullable()
     })
 
-    knex.schema.createTable('yarntype', (table) => {
+    await knex.schema.createTable('yarntype', (table) => {
         table.uuid('id').primary().unique().notNullable()
         table.text('name').notNullable()
         table.text('brand')
@@ -29,20 +31,20 @@ exports.up = function(knex) {
         table.text('notes')
     })
 
-    knex.schema.createTable('yarntype_fiber', (table) => {
+    await knex.schema.createTable('yarntype_fiber', (table) => {
         table.uuid('id').primary().unique().notNullable()
         table.uuid('fiber_id').references('id').inTable('fiber').notNullable()
         table.uuid('yarntype_id').references('id').inTable('yarntype').notNullable()
         table.decimal('percentage')
     })
 
-    knex.schema.createTable('colorway_yarntype', (table) => {
+    await knex.schema.createTable('colorway_yarntype', (table) => {
         table.uuid('id').primary().unique().notNullable()
         table.uuid('colorway_id').references('id').inTable('colorway').unique().notNullable()
         table.uuid('yarntype_id').references('id').inTable('yarntype').notNullable()
     })
 
-    knex.schema.createTable('skein', (table) => {
+    await knex.schema.createTable('skein', (table) => {
         table.uuid('id').primary().unique().notNullable()
         table.uuid('user_id').references('id').inTable('user').notNullable()
         table.uuid('colorway_yarntype_id').references('id').inTable('colorway_yarntype').unique().notNullable()
@@ -50,12 +52,12 @@ exports.up = function(knex) {
     })
 };
 
-exports.down = function(knex) {
-    knex.schema.dropTableIfExists('skein')
-    knex.schema.dropTableIfExists('colorway_yarntype')
-    knex.schema.dropTableIfExists('yarntype_fiber')
-    knex.schema.dropTableIfExists('yarntype')
-    knex.schema.dropTableIfExists('fiber')
-    knex.schema.dropTableIfExists('colorway')
-    knex.schema.dropTableIfExists('user')
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('skein')
+    await knex.schema.dropTableIfExists('colorway_yarntype')
+    await knex.schema.dropTableIfExists('yarntype_fiber')
+    await knex.schema.dropTableIfExists('yarntype')
+    await knex.schema.dropTableIfExists('fiber')
+    await knex.schema.dropTableIfExists('colorway')
+    await knex.schema.dropTableIfExists('user')
 };
