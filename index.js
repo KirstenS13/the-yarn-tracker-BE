c75cd11d7047f6812466e5bf7c61db19f362c73f
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-07-15 16:36:51 -0500

    installed nodemon as a dev dependency and created a server script to run the server using nodemon

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// imports
 const express = require('express');
+
+// create server
 const server = express();
 const port = process.env.PORT || 3000;
 
+// use middleware here
+
+// use routes and routers here
 server.get('/', (req, res) => {
     res.send('Hello, this is the server')
 });
 
+// use error middleware here
+
+// start the server here
 server.listen(port, () => {
     console.log(`The server is running on port ${port}`)
 });
