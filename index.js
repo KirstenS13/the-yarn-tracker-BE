c860142116c3b78be2eba5dbee84481ec815743a
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-07-15 16:43:09 -0500

    added error middleware and middleware to handle incoming json data

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = express();
 const port = process.env.PORT || 3000;
 
 // use middleware here
+// middleware to handle incoming json data
+server.use(express.json());
 
 // use routes and routers here
 server.get('/', (req, res) => {
@@ -13,6 +15,13 @@ server.get('/', (req, res) => {
 });
 
 // use error middleware here
+server.use((err, req, res, next) => {
+    console.log(err)
+
+    res.status(500).json({
+        message: "Something went wrong, please try again later"
+    })
+});
 
 // start the server here
 server.listen(port, () => {
