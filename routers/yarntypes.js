9f97718eb16e9085b1ed5e41de98c634ce18a77a
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-07-19 22:03:36 -0500

    created routers folder and yarntypes.js router file, added skeleton get request for the '/' path that will fetch all yarntypes a user has

---
+++ routers/yarntypes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// the homepage route (for a user who is logged in)
+// the homepage will display all the yarntypes the user has as cards
+router.get('/', (req, res) => {
+    res.send('A list of all yarntypes a user has will be displayed here')
+})
+
+module.exports = router;
