8358ccf961e06756e8f632466364f29924e26249
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-07-21 12:40:27 -0500

    fixed cleanup seed file so that the files are cleaned in the correct order, rolled back the migrations, reran the migrations, reran the seeds

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,9 +1,9 @@
 exports.seed = async function(knex) {
-  await knex('user').truncate()
-  await knex('colorway').truncate()
-  await knex('fiber').truncate()
-  await knex('yarntype').truncate()
-  await knex('yarntype_fiber').truncate()
-  await knex('colorway_yarntype').truncate()
   await knex('skein').truncate()
+  await knex('colorway_yarntype').truncate()
+  await knex('yarntype_fiber').truncate()
+  await knex('yarntype').truncate()
+  await knex('fiber').truncate()
+  await knex('colorway').truncate()
+  await knex('user').truncate()
 };
