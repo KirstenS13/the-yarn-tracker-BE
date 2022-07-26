6087ac65f3dd56ed8e491256561a02e495ab24a6
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-07-25 22:48:58 -0500

    moved all backend files and directories except node_modules and .gitignore into a new 'server' directory in preparation for merging the front-end into the back-end

--- server/routers/yarntype_router.js
+++ server/routers/yarntype_router.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 // the homepage route (for a user who is logged in)
 // the homepage will display all the yarntypes the user has as cards
-router.get('/', async (req, res, next) => {
+router.get('/api/yarntypes', async (req, res, next) => {
     try {
-        const yarntypes = await Yarntype.findYarntypes()
+        const yarntypes = await Yarntype.getAllYarntypes()
         res.json(yarntypes)
     } catch (err) {
         next(err)
